bb62e876201b7e0c990a3e15fd3a3454997a7b7d
taner77 <tcelikim@gmail.com>
2020-06-21 15:54:29 -0400

    from master

--- src/package01/FirstClass.java
+++ src/package01/FirstClass.java
@@ -4,6 +4,9 @@ public class FirstClass {
     public static void main(String[] args) {
         System.out.println("Ilk commit from local repository");
         System.out.println("Master dan yeni bir commit");
-        System.out.println("yeni branc tan commit");
+
+
+
+        System.out.println("Master dan israrci commit");
     }
 }
