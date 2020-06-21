fd040cc94c3d8e9d18562a71582ab3f09bb3439e
taner77 <tcelikim@gmail.com>
2020-06-21 15:56:43 -0400

    from taner

--- src/package01/FirstClass.java
+++ src/package01/FirstClass.java
@@ -3,5 +3,12 @@ package package01;
 public class FirstClass {
     public static void main(String[] args) {
         System.out.println("Ilk commit from local repository");
+
+
+
+
+        System.out.println("from taner branch");
     }
 }
+
+
