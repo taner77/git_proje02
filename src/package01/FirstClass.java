349dbc84263d789e5053471aa46986ba3aa88131
taner77 <tcelikim@gmail.com>
2020-06-21 15:43:01 -0400

    taner den ilk commit

--- src/package01/FirstClass.java
+++ src/package01/FirstClass.java
@@ -3,5 +3,7 @@ package package01;
 public class FirstClass {
     public static void main(String[] args) {
         System.out.println("Ilk commit from local repository");
+        System.out.println("Master dan yeni bir commit");
+        System.out.println("yeni branc tan commit");
     }
 }
