ec4f427ad5ff98984012663504253762a5d926a4
taner77 <tcelikim@gmail.com>
2020-06-21 15:34:08 -0400

    feature branch ten ilk commit

--- src/package01/FirstClass.java
+++ src/package01/FirstClass.java
@@ -3,5 +3,7 @@ package package01;
 public class FirstClass {
     public static void main(String[] args) {
         System.out.println("Ilk commit from local repository");
+
+        System.out.println("Feature adli yeni branch olusturduk");
     }
 }
