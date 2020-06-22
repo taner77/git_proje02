496228d5a66f69398f483f23e479227d6000cb05
taner77 <58541447+taner77@users.noreply.github.com>
2020-06-22 13:41:19 -0400

    Update FirstClass.java

--- src/package01/FirstClass.java
+++ src/package01/FirstClass.java
@@ -3,5 +3,6 @@ package package01;
 public class FirstClass {
     public static void main(String[] args) {
         System.out.println("Ilk commit from local repository");
+        System.out.println("from git master");
     }
 }
